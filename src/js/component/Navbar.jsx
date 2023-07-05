4743fac10f00f994077420e75a58def0565ee8de
Yanfe Pedroza Alberto <38539546+Yanfep@users.noreply.github.com>
2023-07-05 18:13:22 +0000

    update V2

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     return (
          <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="col-4 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <a className="text-white navbar-brand" href="#">
                         <img src="" />LGBTQIA+
                         </a>
-                    <div className="col-6 navbar-collapse" id="navbarTogglerDemo03">
+                    <div className="col-4 collapse navbar-collapse" id="navbarTogglerDemo03">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                  <a className="text-white nav-link active" aria-current="page" href="#">Home</a>
