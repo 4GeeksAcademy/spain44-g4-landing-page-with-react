535d9a249afcfe9a756aa461243003ab937557db
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-07-07 18:57:35 +0000

    props implemented

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,18 +3,15 @@ import Navbar from './Navbar'
 import { Jumbotron } from './Jumbotron'
 import { CardComponent } from './CardComponent.jsx'
 import { FotterComponent } from './FotterComponent.jsx'
-
+import { prideCards } from "../data/cardsInfo";
 
 //create your first component
 const Home = () => {
 	return (
 		<>
 			<Navbar />
-
 			<Jumbotron />
-			<CardComponent />
-
-
+			<CardComponent prideCards={prideCards} />
 			<FotterComponent />
 		</>
 	);
